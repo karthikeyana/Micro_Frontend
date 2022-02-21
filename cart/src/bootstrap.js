b91de6f800a2632458e1fb4455d26d9d4ece4b8a
sony <karthikeyan.a@live.com>
2022-02-21 11:29:54 +0530

    inital commit

---
+++ cart/src/bootstrap.js
@@ -0,0 +1,18 @@
+import faker from 'faker';
+
+const mount = (el) =>{
+    let cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+
+    el.innerHTML = cartText;
+}
+
+if(process.env.NODE_ENV === 'development'){
+    
+    const el = document.querySelector('#dev-cart');
+    
+    if(el){
+        mount(el)
+    }
+}
+
+export {mount};
